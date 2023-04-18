4b2dadda1e7059542d9a418e6081c5fbeae13472
Team Blant <129343439+Projectblant@users.noreply.github.com>
2023-04-18 21:23:52 +0500

    Add files via upload

---
+++ menyaEbalivJopu.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+[{
+	id:1,
+	name:"",
+	years:"",
+	gender:"",
+	teamId:1,
+	positionInGame:"",
+
+}]
+
+
+
+Team[{
+	id:1
+	name:"",
+	AvgLvl:"",
+	CountMembers:228,
+
+}]
